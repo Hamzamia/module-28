4c86202fe24f08284aa0ee9d7494f40c53d2e8d1
Hamzammia <mahmudul.web.developer@gmail.com>
2021-09-04 17:15:57 +0600

    add

---
+++ case-convert.js
@@ -0,0 +1,13 @@
+const anthem = "Amar Sonar Bangla";
+const search = " Amar";
+const userInput = 'Blackpink';
+
+const savedUserName = 'blackpink';
+// console.log(userInput.toLocaleLowerCase());
+// console.log(savedUserName.toLowerCase());
+
+
+if (userInput.toLowerCase() == savedUserName.toLowerCase()) {
+    console.log('user exised');
+}
+
---
+++ string-slice.js
@@ -0,0 +1,24 @@
+const anthem = 'Amar Sonar Bangla Ami Tomai Bhalobasi';
+const word = anthem.split(' ');
+const witha = anthem.split('a');
+// console.log(witha);
+
+// const slicepart = anthem.slice(5, 20);
+
+// substr
+// const substrintest = anthem.substr(55, 5);
+
+// substring test
+const anatherTest = anthem.substring(0, 4);
+
+// console.log(substrintest);
+console.log(anatherTest)
+
+// concat 
+const first = 'amader';
+const second = 'city';
+
+// const full = first + second;
+const full = first.concat(second).concat('abc').concat(' Hamza Mahmud');
+
+console.log(full);
